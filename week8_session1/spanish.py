4cafe0f146c04d27229daa7f36278891b3d089a7
nw <nico.walters@techtalents.es>
2020-06-01 10:54:48 +0200

    NTLK + spaCy

--- week8_session1/spanish.py
+++ week8_session1/spanish.py
@@ -1,12 +1,16 @@
 import spacy
+from nltk.corpus import stopwords
+
 
 es_parser = spacy.load("es_core_news_md")
+es_stopwords = stopwords.words("spanish")
 
 
 oracion = "Python es un lenguaje de programación multiparadigma. Esto significa que más que forzar a los programadores a adoptar un estilo particular de programación, permite varios estilos: programación orientada a objetos, programación imperativa y programación funcional. Otros paradigmas están soportados mediante el uso de extensiones."
 
 parsed = es_parser(oracion)
 
-
+cleaned_words = []
 for token in parsed:
-    print(token, token.pos_, token.lemma_)
+    if not token.text in es_stopwords:
+        cleaned_words.append(token.text)
