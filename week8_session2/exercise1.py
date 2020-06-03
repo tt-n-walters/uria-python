ec47ba9338172c898960d5101507c7af9e2d5485
nw <nico.walters@techtalents.es>
2020-06-03 10:48:27 +0200

    Progress

--- week8_session2/exercise1.py
+++ week8_session2/exercise1.py
@@ -1,7 +1,7 @@
 # Write a Python NLTK program to tokenize sentences in languages other than English.
 # TIP: word_tokenize accepts 2 arguments, the text to tokenize and the language.
 
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 
 text1 = '''
@@ -11,7 +11,9 @@ abgeleiteten Werke 3.0 in den Vereinigten Staaten verteilt.
 '''
 
 de_tokens = word_tokenize(text1, language="german")
+de_sent_tokens = sent_tokenize(text1, language="german")
 print(de_tokens)
+print(de_sent_tokens)
 
 text2 = '''
 Durante su estancia en CNRI, van Rossum lanzó la iniciativa Computer Programming for 
@@ -21,3 +23,6 @@ alfabetización básica en inglés y habilidades matemáticas necesarias por muc
 Python tuvo un papel crucial en este proceso: debido a su orientación hacia una sintaxis 
 limpia, ya era idóneo, y las metas de CP4E presentaban similitudes con su predecesor, ABC.
 '''
+
+es_tokens = sent_tokenize(text2, language="spanish")
+print(es_tokens)
