e1d6653f8cfdcef9f9a44c1b3c179b5ae12ccc2f
nw <nico.walters@techtalents.es>
2020-06-05 11:17:32 +0200

    Defined functions

--- week8_session3/employees.py
+++ week8_session3/employees.py
@@ -1,13 +1,14 @@
 import requests
 
-url = "https://librebor.me/borme/api/v1/empresa/construcciones-uria-alvarez/"
-response = requests.get(url)
-if response.status_code == 200:
-    print(response.text)
+def get_all_info(company_name):
+    url = "https://librebor.me/borme/api/v1/empresa/xxx-xxx-xxx/".replace("xxx-xxx-xxx", company_name)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
 
 
-
-url = "https://libreborme.net/borme/api/v1/empresa/search/?q=uria&page=1"
-response = requests.get(url)
-if response.status_code == 200:
-    print(response.text)
+def search_company(name):
+    url = "https://libreborme.net/borme/api/v1/empresa/search/?q=xxx&page=1".replace("xxx", name)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
