e03a73fc69ea55ed0ee2181481f31f3889142fc3
nw <nico.walters@techtalents.es>
2020-06-05 11:29:46 +0200

    Progress

--- week8_session3/employees.py
+++ week8_session3/employees.py
@@ -1,14 +1,19 @@
 import requests
+import json
 
 def get_all_info(company_name):
     url = "https://librebor.me/borme/api/v1/empresa/xxx-xxx-xxx/".replace("xxx-xxx-xxx", company_name)
     response = requests.get(url)
     if response.status_code == 200:
-        print(response.text)
+        converted = json.loads(response.text)
+        return converted
 
 
 def search_company(name):
     url = "https://libreborme.net/borme/api/v1/empresa/search/?q=xxx&page=1".replace("xxx", name)
     response = requests.get(url)
     if response.status_code == 200:
-        print(response.text)
+        converted = json.loads(response.text)
+        return converted
+
+get_all_info("uria-industrial")
