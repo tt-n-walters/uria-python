5d6ab21a7be98caeb498342fb2621c36a08a8605
nw <nico.walters@techtalents.es>
2020-06-05 11:08:25 +0200

    Company searching

--- week8_session3/employees.py
+++ week8_session3/employees.py
@@ -4,3 +4,10 @@ url = "https://librebor.me/borme/api/v1/empresa/construcciones-uria-alvarez/"
 response = requests.get(url)
 if response.status_code == 200:
     print(response.text)
+
+
+
+url = "https://libreborme.net/borme/api/v1/empresa/search/?q=uria&page=1"
+response = requests.get(url)
+if response.status_code == 200:
+    print(response.text)
