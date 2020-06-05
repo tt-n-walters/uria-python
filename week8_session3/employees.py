d5d878066b0615facc2336410cc327465f961a1d
nw <nico.walters@techtalents.es>
2020-06-05 12:09:22 +0200

    Progress

--- week8_session3/employees.py
+++ week8_session3/employees.py
@@ -20,6 +20,15 @@ def search_company(name):
 print("Enter name to search for:")
 name = input()
 companies = search_company(name)
-company = companies["objects"][0]
-company_name = company["slug"]
-print(company_name)
+objects = companies["objects"]
+
+if len(objects) == 0:
+    print("No results found.")
+else:
+    company = objects[0]
+
+    company_name = company["slug"]
+    company_info = get_all_info(company_name)
+    
+    employees = company_info["cargos_actuales_p"]
+    print("Number of employees is", len(employees))
