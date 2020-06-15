a69c75ba87701719f930a7dff166aa01e172db7b
nw <nico.walters@techtalents.es>
2020-06-15 15:52:23 +0200

    Advanced dictionaries

---
+++ adv_dictionaries.py/adv_dictionaries.py
@@ -0,0 +1,36 @@
+
+hair_colours = {
+    "Arthur": "ginger",
+    "Bill": "ginger",
+    "Charlie": "ginger",
+    "Draco": "blond",
+    "Errol": "feathers",
+    "Fred": "ginger",
+    "George": "ginger",
+    "Harry": "black"
+}
+
+# Accessing items that may not exist
+print(hair_colours.get("Lucius", display_not_found("Lucius")))
+
+# keys, values, items
+print(hair_colours.keys())
+print(hair_colours.values())
+print(hair_colours.items())
+
+for name, colour in hair_colours.items():
+    print(name + " has " + colour + " hair.")
+
+
+
+
+from collections import defaultdict
+
+letters = "TXkOcdCuAaBayuYYPvroKvmYcDCnbYYzugdElReslpflGYEdgAqCWuDsBMQpdjSBUVoVFFJdkJbOrUlKYcROxyDzkDTmmbHYdVXxDcifsoUcoKrgtuwMjXAjIABbOHcL"
+
+times_appeared = defaultdict(int)
+
+for letter in letters:
+    times_appeared[letter] = times_appeared[letter] + 1
+
+print(times_appeared)
